70cdc17046c282f573a506eaad7aeb3a82671e5b
julychn <julychn96@163.com>
2017-07-12 09:44:13 +0800

    Annotation

---
+++ src/ch14/SuppressWarningsTest.java
@@ -0,0 +1,15 @@
+package ch14;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by scn on 2017/7/12.
+ */
+// 关闭整个类得力的编译器警告
+@SuppressWarnings(value = "unchecked")
+public class SuppressWarningsTest {
+        public static void main(String[] args) {
+            List<String> myList = new ArrayList();
+        }
+}
